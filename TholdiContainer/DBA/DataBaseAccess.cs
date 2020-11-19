d2c8e83c8796dc6009621c560f1587993aa7cfd1
Alexis Ottina <masdevelopper@gmail.com>
2020-11-19 13:20:00 +0100

    :construction: DataBaseAccess
    Classe pour se conncter à la bdd (nécessite MySql.Data)

---
+++ TholdiContainer/DBA/DataBaseAccess.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace TholdiContainer.DBA
+{
+    class DataBaseAccess
+    {
+        public static MySqlConnection getOpenMySqlConnection()
+        {
+            MySqlConnection msc = new MySqlConnection(
+                "Database=mydb_aott;" +
+                "Data Source=217.167.171.227;" +
+                "UserId=aott;Password=12/11/1998;" +
+                "Ssl Mode=None;" +
+                "charset=utf8");
+
+            msc.Open();
+            return msc;
+        }
+    }
+}
