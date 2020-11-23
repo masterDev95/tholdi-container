0df11a1c08b468be73f5fd32c2b539347418bc1a
Alexis Ottina <masdevelopper@gmail.com>
2020-11-23 15:04:26 +0100

    :sparkles: Probleme Active Record

---
+++ TholdiContainer/Tables/Probleme.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using TholdiContainer.DBA;
+
+namespace TholdiContainer.Tables
+{
+    class Probleme
+    {
+        private static string selectSql = "SELECT * FROM PROBLEME";
+        private static string selectByIdSql = "SELECT * FROM PROBLEME WHERE CODEPROBLEME = ?CodeProbleme ";
+        private static string updateSql = "UPDATE PROBLEME SET LIBELLEPROBLEME=?LibelleProbleme WHERE CODEPROBLEME=?CodeProbleme";
+        private static string insertSql = "INSERT INTO PROBLEME (CODEPROBLEME, LIBELLEPROBLEME) VALUES (?CodeProbleme, ?LibelleProbleme)";
+
+        private bool isNew;
+
+        public short CodeProbleme { get; private set; }
+        public string LibelleProbleme { get; set; }
+
+        public Probleme(short codeProbleme)
+        {
+            this.CodeProbleme = codeProbleme;
+            this.isNew = true;
+        }
+
+        static public Probleme Fetch(int codeProbleme)
+        {
+            Probleme unProbleme = null;
+            MySqlConnection openConnection = DataBaseAccess.getOpenMySqlConnection();
+            MySqlCommand commandSql = openConnection.CreateCommand();
+
+            commandSql.CommandText = Probleme.selectByIdSql;
+            commandSql.Parameters.Add(new MySqlParameter("?CodeProbleme", codeProbleme));
+            commandSql.Prepare();
+
+            MySqlDataReader jeuEnregistrements = commandSql.ExecuteReader();
+            bool existEnregistrement = jeuEnregistrements.Read();
+
+            if (existEnregistrement)
+            {
+                unProbleme = new Probleme(short.Parse(jeuEnregistrements["CodeProbleme"].ToString()))
+                {
+                    LibelleProbleme = jeuEnregistrements["LibelleProbleme"].ToString(),
+                    isNew = false
+                };
+            }
+
+            openConnection.Close();
+            return unProbleme;
+        }
+
+        static public List<Probleme> FetchAll()
+        {
+            List<Probleme> resultat = new List<Probleme>();
+            MySqlConnection openConnection = DataBaseAccess.getOpenMySqlConnection();
+            MySqlCommand commandSql = openConnection.CreateCommand();
+
+            commandSql.CommandText = Probleme.selectSql;
+
+            MySqlDataReader jeuEnregistrements = commandSql.ExecuteReader();
+
+            while (jeuEnregistrements.Read())
+            {
+                Probleme unProbleme = new Probleme(short.Parse(jeuEnregistrements["CodeProbleme"].ToString()))
+                {
+                    LibelleProbleme = jeuEnregistrements["LibelleProbleme"].ToString(),
+                    isNew = false,
+                };
+
+                resultat.Add(unProbleme);
+            }
+
+            openConnection.Close();
+            return resultat;
+        }
+
+        private void Insert()
+        {
+            MySqlConnection openConnection = DataBaseAccess.getOpenMySqlConnection();
+            MySqlCommand commandSql = openConnection.CreateCommand();
+
+            commandSql.CommandText = Probleme.insertSql;
+            commandSql.Parameters.Add(new MySqlParameter("?CodeProbleme", this.CodeProbleme));
+            commandSql.Parameters.Add(new MySqlParameter("?LibelleProbleme", this.LibelleProbleme));
+            commandSql.Prepare();
+            commandSql.ExecuteNonQuery();
+
+            openConnection.Close();
+        }
+
+        private void Update()
+        {
+            MySqlConnection openConnection = DataBaseAccess.getOpenMySqlConnection();
+            MySqlCommand commandSql = openConnection.CreateCommand();
+
+            commandSql.CommandText = Probleme.updateSql;
+            commandSql.Parameters.Add(new MySqlParameter("?CodeProbleme", this.CodeProbleme));
+            commandSql.Parameters.Add(new MySqlParameter("?LibelleProbleme", this.LibelleProbleme));
+            commandSql.Prepare();
+            commandSql.ExecuteNonQuery();
+
+            openConnection.Close();
+        }
+
+        public void Save()
+        {
+            if (isNew)
+                Insert();
+            else
+                Update();
+        }
+    }
+}
